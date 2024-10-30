7950fb7fe7acc1a80ea395be3dee6cea35d1c5d3
Gleb Kuzmin <kuzmin@owlcat.games>
2024-10-30 23:29:33 +0100

    Add netcode to project

---
+++ Assets/ZombieShooter/Code/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace ZombieShooter
+{
+    [Serializable]
+    public class PlayerData
+    {
+        public string Name;
+        public Color MainColor;
+        public Color AdditionalColor;
+    }
+}
